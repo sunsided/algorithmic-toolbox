978052ee4c15fc50d42b7e9a0a8e0d47f8f720a0
Markus Mayer <widemeadows@gmail.com>
2019-03-21 20:20:05 +0100

    Update problem 1 solution

--- week1_programming_challenges/1_sum_of_two_digits/APlusB.cs
+++ week1_programming_challenges/1_sum_of_two_digits/APlusB.cs
@@ -2,9 +2,9 @@ using System;
 
 namespace APlusB
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             var input = Console.ReadLine();
             var tokens = input.Split(' ');

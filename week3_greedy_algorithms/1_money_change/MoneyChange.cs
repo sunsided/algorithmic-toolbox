20a64936a017a7a7537cbff1b961ae44698331e2
Markus Mayer <widemeadows@gmail.com>
2019-03-31 01:16:52 +0100

    Add Week 3: Money change

---
+++ week3_greedy_algorithms/1_money_change/MoneyChange.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Diagnostics;
+
+namespace project
+{
+    internal static class Program
+    {
+        // ReSharper disable once UnusedParameter.Local
+        private static void Main(string[] args)
+        {
+            int n;
+            ParseInputs(out n);
+            var solution = FastSolution(n);
+            Console.WriteLine(solution);
+        }
+        
+        private static int FastSolution(int n)
+        {
+            var coins = 0;
+            while (n >= 10)
+            {
+                n -= 10;
+                ++coins;
+            }
+            
+            while (n >= 5)
+            {
+                n -= 5;
+                ++coins;
+            }
+            
+            while (n >= 1)
+            {
+                --n;
+                ++coins;
+            }
+
+            return coins;
+        }
+
+        private static void ParseInputs(out int n)
+        {
+            // Read number of inputs
+            var input = Console.ReadLine();
+            Debug.Assert(input != null, "input != null");
+            var values = input.Split();
+            n = int.Parse(values[0]);
+        }
+    }
+}

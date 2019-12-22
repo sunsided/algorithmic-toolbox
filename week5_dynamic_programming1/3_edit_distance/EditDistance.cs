1c53d5b42de6c9533a47151ab336566cec6ddc45
Markus Mayer <widemeadows@gmail.com>
2019-12-22 11:17:28 +0100

    Prepare Week 5: Edit Distance

---
+++ week5_dynamic_programming1/3_edit_distance/EditDistance.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace EditDistance
+{
+    internal static class Program
+    {
+        // ReSharper disable once UnusedParameter.Local
+        private static void Main(string[] args)
+        {
+#if TESTING
+#else
+            string first, second;
+            ParseInputs(out first, out second);
+            var solution = Solution(first, second);
+            Console.WriteLine(solution);
+#endif
+        }
+
+        private static int Solution(string first, string second)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ParseInputs(out string first, out string second)
+        {
+            first = Console.ReadLine();
+            second = Console.ReadLine();
+        }
+    }
+}
